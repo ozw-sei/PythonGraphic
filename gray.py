f5606ce6b76d65fecfcba8a848d5bb57bd2329b8
Ozawa Seijiro <ozawa4751@gmail.com>
2013-05-26 02:56:52 +0900

    init

---
+++ gray.py
@@ -0,0 +1,8 @@
+#coding-utf-8
+from PIL import Image
+from PIL import ImageOps
+
+if __name__ == "__main__":
+    input_img = Image.open("lena.png")
+    output_img = ImageOps.grayscale(input_img)
+    output_img.save("gray_lena.png")
